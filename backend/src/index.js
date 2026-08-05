1185ac8d0ddb066655fc984c4be7ba58dd6bc0e6
itsdat <vinhdat998@gmail.com>
2026-08-05 10:17:41 +0700

    update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,11 +27,11 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(process.cwd(), "frontend/dist")))
 
     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+        res.sendFile(path.join(process.cwd(), "frontend/dist", "index.html"))
     })
 }
 
