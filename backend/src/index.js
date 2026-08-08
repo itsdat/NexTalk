82ff87a369d604d036008db11986e0b98d463d6c
itsdat <vinhdat998@gmail.com>
2026-08-08 08:35:23 +0700

    update check status api

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,41 +1,44 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
-import cors from "cors"
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
 import { app, server } from "./lib/socket.js";
-import path from "path"
+import path from "path";
 
+dotenv.config();
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-dotenv.config()
-const PORT = process.env.PORT
-const __dirname = path.resolve()
+app.use(cookieParser());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.use(cookieParser())
-app.use(express.json({ limit: "50mb" })); 
-app.use(express.urlencoded({ limit: "50mb", extended: true })); 
-
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
-    credentials: true
-}))
+    credentials: true,
+  }),
+);
 
-app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes)
-
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok", messages: "Server working" });
+});
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(process.cwd(), "frontend/dist")))
+  app.use(express.static(path.join(process.cwd(), "frontend/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(process.cwd(), "frontend/dist", "index.html"))
-    })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(process.cwd(), "frontend/dist", "index.html"));
+  });
 }
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
-})
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
